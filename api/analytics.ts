c54c676556042938689c2448ad345e8e23f6d7f5
Prophet-Bestman <bestiproph@gmail.com>
2023-04-06 04:59:28 +0100

    Updated link to api

--- api/analytics.ts
+++ api/analytics.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const request = axios.create({
-  baseURL: "https://test.api.mainstack.io",
+  baseURL: "https://fe-task-api.mainstack.io/",
 });
 
 export const useGetAnalytics = () => {
